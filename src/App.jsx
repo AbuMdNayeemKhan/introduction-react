34b5b0b85530e9c312aaedb34d88c00adb0173bd
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-03-08 01:33:02 +0600

    sending data by distrucring in react js.

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
     <>
       <h1 className='student'>Hello World!</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia unde laborum ad voluptas, dolore in illum culpa aspernatur ex velit nobis, totam, nam fuga nihil at necessitatibus sequi sint maiores est laboriosam consequatur. Accusamus, possimus corporis sint a sequi pariatur? Tempora dolor ratione officiis ullam aliquid nesciunt, quibusdam, unde minima modi eius enim atque consequatur vitae ipsa! Ratione ducimus magni amet at rem, explicabo pariatur, aut voluptas quod sint eveniet suscipit, quos dolorum! Nihil distinctio doloribus porro soluta incidunt, reprehenderit provident odio temporibus quidem corrupti similique et a, atque vero fugit iste asperiores adipisci nobis ex? Veritatis omnis odio quod?</p>
+      <Students name='Nayeem' score='Nine'></Students>
       <Device name='Nokia 3.4' price='12500'></Device>
       <Device name='Mackbook air' price='112000'></Device>
       <Device name='Mack mini' price='66000'></Device>
@@ -21,6 +22,17 @@ function App() {
 function Device(props){
   return(
     <h1>The product name is: {props.name}. and price is: {props.price}.</h1>
+  )
+}
+
+function Students({name, score}){
+  const studentsStyle = {
+    padding: '10px',
+    marginBottom: '10px',
+    backgroundColor: 'gray',
+  }
+  return (
+    <h1 style={studentsStyle}>My Name is {name}. I'm class {score}</h1>
   )
 }
 
