8b9bafc5714b69a04c53424efce839eef6cc19ba
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-03-07 22:57:54 +0600

    use css inside the react component

--- src/App.jsx
+++ src/App.jsx
@@ -8,27 +8,25 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className='student'>Hello World!</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia unde laborum ad voluptas, dolore in illum culpa aspernatur ex velit nobis, totam, nam fuga nihil at necessitatibus sequi sint maiores est laboriosam consequatur. Accusamus, possimus corporis sint a sequi pariatur? Tempora dolor ratione officiis ullam aliquid nesciunt, quibusdam, unde minima modi eius enim atque consequatur vitae ipsa! Ratione ducimus magni amet at rem, explicabo pariatur, aut voluptas quod sint eveniet suscipit, quos dolorum! Nihil distinctio doloribus porro soluta incidunt, reprehenderit provident odio temporibus quidem corrupti similique et a, atque vero fugit iste asperiores adipisci nobis ex? Veritatis omnis odio quod?</p>
+      <Devlopers></Devlopers>
     </>
+  )
+}
+
+function Devlopers(){
+  const devStyle = {
+    margin: '20px',
+    padding: '20px',
+    border: '2px solid purple',
+    borderRadius: '20px'
+  }
+  return(
+    <div style={devStyle}>
+      <h5>Devo devo</h5>
+      <p>Coding: </p>
+    </div>
   )
 }
 
