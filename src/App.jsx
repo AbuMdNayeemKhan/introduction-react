c81e9661cc4db2e9188d43ea13cc1996a90b28b4
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-03-07 23:02:49 +0600

    react data pass by props

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,17 @@ function App() {
     <>
       <h1 className='student'>Hello World!</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia unde laborum ad voluptas, dolore in illum culpa aspernatur ex velit nobis, totam, nam fuga nihil at necessitatibus sequi sint maiores est laboriosam consequatur. Accusamus, possimus corporis sint a sequi pariatur? Tempora dolor ratione officiis ullam aliquid nesciunt, quibusdam, unde minima modi eius enim atque consequatur vitae ipsa! Ratione ducimus magni amet at rem, explicabo pariatur, aut voluptas quod sint eveniet suscipit, quos dolorum! Nihil distinctio doloribus porro soluta incidunt, reprehenderit provident odio temporibus quidem corrupti similique et a, atque vero fugit iste asperiores adipisci nobis ex? Veritatis omnis odio quod?</p>
+      <Device name='Nokia 3.4' price='12500'></Device>
+      <Device name='Mackbook air' price='112000'></Device>
+      <Device name='Mack mini' price='66000'></Device>
       <Devlopers></Devlopers>
     </>
+  )
+}
+
+function Device(props){
+  return(
+    <h1>The product name is: {props.name}. and price is: {props.price}.</h1>
   )
 }
 
