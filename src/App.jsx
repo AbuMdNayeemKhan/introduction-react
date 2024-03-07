b1babd236037a494adc24389e675b08bf16fbe1a
AbuMdNayeemKhan <abumohammadnayemkhan@gmail.com>
2024-03-08 02:08:54 +0600

    create a component and adde data

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './Todo'
+import Todo from './Todo'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='student'>Hello World!</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia unde laborum ad voluptas, dolore in illum culpa aspernatur ex velit nobis, totam, nam fuga nihil at necessitatibus sequi sint maiores est laboriosam consequatur. Accusamus, possimus corporis sint a sequi pariatur? Tempora dolor ratione officiis ullam aliquid nesciunt, quibusdam, unde minima modi eius enim atque consequatur vitae ipsa! Ratione ducimus magni amet at rem, explicabo pariatur, aut voluptas quod sint eveniet suscipit, quos dolorum! Nihil distinctio doloribus porro soluta incidunt, reprehenderit provident odio temporibus quidem corrupti similique et a, atque vero fugit iste asperiores adipisci nobis ex? Veritatis omnis odio quod?</p>
-      <Students name='Nayeem' score='Nine'></Students>
-      <Device name='Nokia 3.4' price='12500'></Device>
-      <Device name='Mackbook air' price='112000'></Device>
-      <Device name='Mack mini' price='66000'></Device>
-      <Devlopers></Devlopers>
+      <Todo toDo='Learn More about react.'></Todo>
+      <Todo toDo='Learn More about Javascipt.'></Todo>
     </>
   )
 }
